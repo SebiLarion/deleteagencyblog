ebacb475b3057505adb5845a3507c98065ad0170
Eusebiu Larion <sebilarion@gmail.com>
2020-09-23 21:29:13 +0300

    first commit

---
+++ dist/script.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+    $(".nav-toggler").each(function(_, navToggler) {
+      var target = $(navToggler).data("target");
+      $(navToggler).on("click", function() {
+        $(target).animate({
+          height: "toggle"
+         
+        });
+      });
+    });
+  });
+
+  
+  
